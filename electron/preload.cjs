ea53b128bc14a6ffa530e265896929ff4dbd8131
Findupto <11663892+findupto@users.noreply.github.com>
2026-08-28 17:27:21 +0500

    Expose YD801 Bluetooth serial diagnostic test

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,2 +1,2 @@
 const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('mkPosDesktop',{isDesktop:true,discoverPrinters:()=>ipcRenderer.invoke('printer:list'),printEscPos:p=>ipcRenderer.invoke('printer:print',p),printRaw:p=>ipcRenderer.invoke('printer:print-raw',p),printHtml:p=>ipcRenderer.invoke('printer:print-html',p)});
+contextBridge.exposeInMainWorld('mkPosDesktop',{isDesktop:true,discoverPrinters:()=>ipcRenderer.invoke('printer:list'),printEscPos:p=>ipcRenderer.invoke('printer:print',p),testSerialPrinter:p=>ipcRenderer.invoke('printer:test-serial',p),printRaw:p=>ipcRenderer.invoke('printer:print-raw',p),printHtml:p=>ipcRenderer.invoke('printer:print-html',p)});
