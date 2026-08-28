69565ab0c4a606f313c0a5239a0b312a2d1fc076
Findupto <11663892+findupto@users.noreply.github.com>
2026-08-28 22:06:56 +0500

    feat: expose secure local database bridge

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,2 +1,19 @@
 const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('mkPosDesktop',{isDesktop:true,discoverPrinters:()=>ipcRenderer.invoke('printer:list'),bluetoothSend:p=>ipcRenderer.invoke('printer:bluetooth-send',p),printEscPos:p=>ipcRenderer.invoke('printer:print',p),testSerial:p=>ipcRenderer.invoke('printer:test-serial',p),testSerialPrinter:p=>ipcRenderer.invoke('printer:test-serial',p),printRaw:p=>ipcRenderer.invoke('printer:print-raw',p),printHtml:p=>ipcRenderer.invoke('printer:print-html',p)});
+
+const db = {
+  loadState: () => ipcRenderer.invoke('db:load-state'),
+  saveState: (state, actor) => ipcRenderer.invoke('db:save-state', { state, actor }),
+  backup: (targetPath) => ipcRenderer.invoke('db:backup', { targetPath })
+};
+
+contextBridge.exposeInMainWorld('mkPosDesktop', {
+  isDesktop: true,
+  database: db,
+  discoverPrinters: () => ipcRenderer.invoke('printer:list'),
+  bluetoothSend: p => ipcRenderer.invoke('printer:bluetooth-send', p),
+  printEscPos: p => ipcRenderer.invoke('printer:print', p),
+  testSerial: p => ipcRenderer.invoke('printer:test-serial', p),
+  testSerialPrinter: p => ipcRenderer.invoke('printer:test-serial', p),
+  printRaw: p => ipcRenderer.invoke('printer:print-raw', p),
+  printHtml: p => ipcRenderer.invoke('printer:print-html', p)
+});
